c5662a23e7bc059f9de893e3352ff5347e5a5ff1
expo94718 <wwwjinfood1714@gmail.com>
2023-05-20 00:43:45 +0900

    use lombok

--- src/main/java/net/starly/waiter/WaiterMain.java
+++ src/main/java/net/starly/waiter/WaiterMain.java
@@ -1,17 +1,15 @@
 package net.starly.waiter;
 
+import lombok.Getter;
 import net.starly.core.bstats.Metrics;
 import net.starly.waiter.runnable.TimeCheckSchedule;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class WaiterMain extends JavaPlugin {
+public final class WaiterMain extends JavaPlugin  {
 
+    @Getter
     private static JavaPlugin instance;
-
-    public static JavaPlugin getInstance() {
-        return instance;
-    }
 
     public void onLoad() {
         instance = this;
